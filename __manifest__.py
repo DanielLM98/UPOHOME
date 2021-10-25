0ef6b220b312705df01eaed2c67a8df87d3df1da
DanielLM98 <33654417+DanielLM98@users.noreply.github.com>
2021-10-25 18:50:40 +0200

    Estructura base creada
    Estructura base de un modulo odoo

---
+++ __manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "upohome",
+
+    'summary': """
+        Sumario del modulo GYM""",
+
+    'description': """
+        UPOHOME el modulo que te permitirá controlar 
+    """,
+
+    'author': "Daniel Luna, Carlos Aranada, Aitor Muñoz, Miguel Pavon, Alvaro Pazos",
+    'category': 'Rental, Home',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+    ],
+}
